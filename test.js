03bf0479f505749e459b89773e73ae53f7a95f49
shiying <tshiying0213@gmail.com>
2025-08-27 13:14:51 +0800

    changes

---
+++ test.js
@@ -0,0 +1,17 @@
+// Sample JavaScript code
+
+// Function to add two numbers
+function addNumbers(a, b) {
+    return a + b;
+}
+
+// Function to greet a user
+function greetUser(name) {
+    console.log(`Hello, ${name}!`);
+}
+
+// Example usage
+const sum = addNumbers(5, 10);
+console.log(`The sum is: ${sum}`);
+
+greetUser("Enduser");
